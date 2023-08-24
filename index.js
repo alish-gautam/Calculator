a3b8068cf43d4f830b9870c29dd680d37e258bc2
Alish Gautam <alishgautam890@gmail.com>
2023-08-24 08:24:12 +0545

    Calculator Project Completed

--- index.js
+++ index.js
@@ -1,78 +1,65 @@
 //Getting all the variables required
 let number=document.querySelectorAll(".number")
-let operator=document.querySelectorAll(".operator")
+let operation=document.querySelectorAll(".operator")
 let display=document.querySelector(".display")
 let clear=document.querySelector(".clear")
 let equals=document.querySelector(".equals")
 
 // declaring values and operator
-let firstNum=""
+let previousNum=""
 let myoperator=""
-let secondNum=""
+let currentNum=""
 
-// function for when a number button is clicked..it takes first and second number
-number.forEach((num)=>{
-    num.addEventListener("click",()=>{
-        if(myoperator==""){
-            firstNum+=num.value
-            display.value=firstNum
-        }
-        else{
-            secondNum+=num.value
-            display.value=secondNum
-            
-        }
-        
-        
-    })
-})
-// getting the operator clicked and displaying it 
-operator.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        if (myoperator !== "") {
-            operate(firstNum, myoperator, secondNum); // Calculate the previous operation
-            firstNum = display.value; // Update firstNum with the result
-            secondNum = ""; // Reset secondNum for a new operand
-        }
-        myoperator=item.innerHTML
-        display.value=myoperator
-
+// running through every item of the number Array
+number.forEach(num=>{
+    num.addEventListener("click",(e)=>{
+        handleNum(e.target.textContent);
     })
 })
 
-function operate(firstNum,myoperator,secondNum){
-    if(myoperator=="+"){
-        if( secondNum===""){
-            secondNum="0"
-        }
-        let result=add(parseFloat(firstNum),parseFloat(secondNum))
-        display.value=result
-        firstNum = result;
-    }
-    if(myoperator=="*"){
-        if(secondNum===""){
-            secondNum="1"
-        }
-        let result=multiply(parseFloat(firstNum),parseFloat(secondNum))
-        display.value=result
-        firstNum = result;
-    }
-   
+// displaying the number on the screen
+function handleNum(num){
+    currentNum+=num
+    display.value=currentNum
 }
-
-// function to calculate result when = button is clicked
-equals.addEventListener("click",()=>{
-    operate(firstNum,myoperator,secondNum)
+// running through every item of the operator Array
+operation.forEach(item=>{
+    item.addEventListener("click",(e)=>{
+        handleOperator(e.target.textContent)
+        display.value=previousNum+""+myoperator
+    })
+})
+// displaying the operator on the screen and changing current and previous values
+function handleOperator(op){
+    myoperator=op
+    previousNum=currentNum
+    currentNum=""
+}
+equals.addEventListener("click",calculate)
+clear.addEventListener("click",()=>{
+    previousNum=""
+    currentNum=""
+    display.value=currentNum
+})
+function calculate(){
+    currentNum=Number(currentNum)
+    previousNum=Number(previousNum)
+    if(myoperator==="+"){
+        // previousNum+=currentNum
+        display.value=previousNum+currentNum
+        currentNum=display.value
+    }
+    if(myoperator==="-"){
+        display.value=previousNum-currentNum
+        currentNum=display.value
+    }
+    if(myoperator==="/"){
+        display.value=previousNum/currentNum
+        currentNum=display.value
+    }
+    if(myoperator==="*"){
+        display.value=previousNum*currentNum
+        currentNum=display.value
+    }
     
-})
-//add function to add the values
-function add(first,second){
-    return first + second
 }
-function multiply(first,second){
-    return first*second
-}
-
-// clearing the display
-clear.addEventListener("click",()=>display.value="")
-
